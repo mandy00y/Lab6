3445b713354c0fe7d2a08b3656e4bdeed25862b1
Your Name <you@example.com>
2022-05-19 13:16:55 -0700

    structured-english

--- lab6.py
+++ lab6.py
@@ -1,3 +1,16 @@
 
 
 
+"""
+To start, we will generate a random integer between 1 and 20, and 
+based on the result of the random number, we check to see if it falls under a certain range
+if the number is greater than 15, then the result will be "Cherries"
+otherwise if the number is > 10, then the rsult will br "Orange"
+otherwise if the number is > 5, then the result will be "Plum"
+otherwise if the number is > 2, then the result will be "Melon"
+othersie if the number is > 1, then the result will be "Bell"
+if the number is not any of the above, then the result will be "Bar"
+
+we iterate over using a loop thrre times and print the results to the user. 
+As an example "Plum Cherries Melon"
+"""
