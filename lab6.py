08e78efb9638db41498ac536283179c40ed102e2
Your Name <you@example.com>
2022-05-19 13:23:51 -0700

    pseudo-code

--- lab6.py
+++ lab6.py
@@ -9,8 +9,32 @@ otherwise if the number is > 10, then the rsult will br "Orange"
 otherwise if the number is > 5, then the result will be "Plum"
 otherwise if the number is > 2, then the result will be "Melon"
 othersie if the number is > 1, then the result will be "Bell"
-if the number is not any of the above, then the result will be "Bar"
+otherwise the number is not any of the above, then the result will be "Bar"
 
 we iterate over using a loop thrre times and print the results to the user. 
 As an example "Plum Cherries Melon"
+"""
+
+
+
+"""
+import random
+num = generate random number 
+
+if num is greater than 15, 
+    then the result will be "Cherries"
+otherwise if the num is > 10,
+    then the rsult will br "Orange"
+otherwise if the num is > 5, 
+    then the result will be "Plum"
+otherwise if the num is > 2, 
+    then the result will be "Melon"
+othersie if the num is > 1, 
+    then the result will be "Bell"
+otherwise 
+    the result will be "Bar"
+
+loop three times
+    print the output (fruit) to user
+    
 """
